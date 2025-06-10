e2ffbc6168a2d66b015c09d96c3c8dbdce53e73e
Richard Braakman <rebraakman@gmail.com>
2025-06-10 14:44:11 +0300

    Combustion web page
    To use, run:
    
    cd combustion-tools
    wasm-pack build --target web
    simple-http-server -p 8008 --nocache --cors .
    
    Then open http://localhost:8008 in a browser.

--- combustion-tools/src/lib.rs
+++ combustion-tools/src/lib.rs
@@ -1,13 +1,14 @@
-use std::io::{Cursor, Result, Write, Error, ErrorKind};
+use std::io::{Cursor, Write, Error, ErrorKind};
 
 use fatfs::ReadWriteSeek;
 use include_dir::{Dir, include_dir};
+use wasm_bindgen::prelude::*;
 
 static IMAGE_SIZE: usize = 1 << 20; // one MiB
 static INPUT_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/image_files");
 
 /// Copy the initial statically-defined set of files into the FAT image.
-fn populate_image<T: ReadWriteSeek>(root: &mut fatfs::Dir<T>, input_dir: &Dir) -> Result<()> {
+fn populate_image<T: ReadWriteSeek>(root: &mut fatfs::Dir<T>, input_dir: &Dir) -> Result<(), Error> {
     for file in input_dir.files() {
         let path = file.path().as_os_str().to_string_lossy();
         let mut fat_file = root.create_file(&path)?;
@@ -26,9 +27,7 @@ fn populate_image<T: ReadWriteSeek>(root: &mut fatfs::Dir<T>, input_dir: &Dir) -
     Ok(())
 }
 
-/// Create a FAT image of `IMAGE_SIZE` size,
-/// populated with files supplied at compile time.
-fn init_image() -> Result<Vec<u8>> {
+fn init_image_internal() -> Result<Vec<u8>, Error> {
     let mut seekable_image = Cursor::new(vec![0; IMAGE_SIZE]);
     let options = fatfs::FormatVolumeOptions::new().volume_label(*b"combustion ");
     fatfs::format_volume(&mut seekable_image, options)?;
@@ -44,17 +43,9 @@ fn init_image() -> Result<Vec<u8>> {
     Ok(seekable_image.into_inner())
 }
 
-/// Scaffolding; will be replaced with WASM calls later.
-fn main() -> Result<()> {
-    let image = init_image()?;
-    let written = std::fs::OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open("test.img")?
-        .write(&image)?;
-    if written != image.len() {
-        eprintln!("error: wrote only {written} bytes of {}", image.len());
-    }
-    Ok(())
+#[wasm_bindgen]
+/// Create a FAT image of `IMAGE_SIZE` size,
+/// populated with files supplied at compile time.
+pub fn init_image() -> Result<Vec<u8>, String> {
+    init_image_internal().map_err(|e| format!("{e}"))
 }
